bbdc447c741a7598e71139815aac2def779364da
Bhavesh Jha <bhaveshjha2002@gmail.com>
2023-02-22 18:24:52 +0530

    some changes in case sensitivity.

--- ResourceManagerAPI/Controllers/FiltersController.cs
+++ ResourceManagerAPI/Controllers/FiltersController.cs
@@ -47,10 +47,10 @@ namespace ResourceManagerAPI.Controllers
                             };
 
             var employee = tempemployee.Where(e => tempskill.Any(s => (s.EmailID == e.EmailID)  &&
-    ((filter.Skill != null && s.Skill == filter.Skill) || (filter.Skill == null))) &&
-    ((filter.Name != null && e.ResourceName == filter.Name) || (filter.Name == null)) &&
-    ((filter.EmailID != null && e.EmailID == filter.EmailID) || (filter.EmailID == null)) &&
-    ((filter.TaskName != null && e.TaskName == filter.TaskName) || (filter.TaskName == null)) &&
+    ((filter.Skill != null && s.Skill.ToUpper() == filter.Skill.ToUpper()) || (filter.Skill == null || filter.Skill == ""))) &&
+    ((filter.Name != null && e.ResourceName.ToUpper() == filter.Name.ToUpper()) || (filter.Name == null || filter.Name == "")) &&
+    ((filter.EmailID != null && e.EmailID.ToUpper() == filter.EmailID.ToUpper()) || (filter.EmailID == null || filter.EmailID == "")) &&
+    ((filter.TaskName != null && e.TaskName.ToUpper() == filter.TaskName.ToUpper()) || (filter.TaskName == null || filter.TaskName == "")) &&
     ((filter.AssignedFrom !=null && e.Start>=filter.AssignedFrom && e.Start <= filter.AssignedTo) || (filter.AssignedFrom == null && filter.AssignedTo == null)) &&
     ((filter.AvailableFrom != null && e.Finish >= filter.AvailableFrom && e.Finish <= filter.AvailableTo) || (filter.AvailableFrom == null && filter.AvailableTo == null))
     ).ToList();
