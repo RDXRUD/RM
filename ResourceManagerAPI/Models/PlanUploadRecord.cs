af46c2126cfa200536449fa0b651d81900b42f30
Bhavesh Jha <bhaveshjha2002@gmail.com>
2023-03-15 14:16:39 +0530

    planuploadrecord added

--- ResourceManagerAPI/Models/PlanUploadRecord.cs
+++ ResourceManagerAPI/Models/PlanUploadRecord.cs
@@ -1,6 +1,12 @@
-﻿namespace ResourceManagerAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceManagerAPI.Models
 {
     public class PlanUploadRecord
     {
+        [Key]
+        public int UserID { get; set; }
+        public string? UserName { get; set; }
+        public string? FileName { get; set; }
     }
 }
